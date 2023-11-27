9826773437b9f4ebc71e9b21fd757ef860e56a8e
MarcosHBlanco <marcos.blanco@al.infnet.edu.br>
2023-11-27 06:51:06 -0300

    Editado branch logs - arquivo ConversorTeste

--- src/test/java/org/example/ConversorTeste.java
+++ src/test/java/org/example/ConversorTeste.java
@@ -1,37 +1,37 @@
-//package org.example;
-//
-//import org.junit.jupiter.api.*;
-//import org.opentest4j.AssertionFailedError;
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//
-//public class ConversorTeste {
-//    private static final Logger LOGGER = LoggerFactory.getLogger(ConversorTeste.class);
-//
-//    @Test
-//    public void test1() {
-//        Conversor conversor = new Conversor();
-//        LOGGER.info("Início do Teste 1");
-//        double resultado = Conversor.metrosParaMilhas(5000);
-//        LOGGER.debug("O valor de resultado é:" + resultado);
-//        assertEquals(3.05, resultado);
-//        resultado = Conversor.metrosParaMilhas(10000);
-//        LOGGER.debug("O valor de resultado é:" + resultado);
-//        assertEquals(6.1, resultado);
-//        LOGGER.info("Fim do Teste 1");
-//    }
-//    @Test
-//    public void test2(){
-//            Conversor conversor = new Conversor();
-//            LOGGER.info("Início do Teste 2");
-//            double resultado = conversor.metrosParaMilhas(5000);
-//            LOGGER.debug("O valor de resultado é:" + resultado);
-//            assertThrows(AssertionFailedError.class, () -> {
-//                assertEquals(1, resultado);
-//            });
-//            LOGGER.error("Exceção foi jogada, nenhum erro.");
-//            LOGGER.info("Fim do Teste 2");
-//        }
-//    }
+package org.example;
+
+import org.junit.jupiter.api.*;
+import org.opentest4j.AssertionFailedError;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ConversorTeste {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ConversorTeste.class);
+
+    @Test
+    public void test1() {
+        Conversor conversor = new Conversor();
+        LOGGER.info("Início do Teste 1");
+        double resultado = Conversor.metrosParaMilhas(5000);
+        LOGGER.debug("O valor de resultado é:" + resultado);
+        assertEquals(3.05, resultado);
+        resultado = Conversor.metrosParaMilhas(10000);
+        LOGGER.debug("O valor de resultado é:" + resultado);
+        assertEquals(6.1, resultado);
+        LOGGER.info("Fim do Teste 1");
+    }
+    @Test
+    public void test2(){
+            Conversor conversor = new Conversor();
+            LOGGER.info("Início do Teste 2");
+            double resultado = conversor.metrosParaMilhas(5000);
+            LOGGER.debug("O valor de resultado é:" + resultado);
+            assertThrows(AssertionFailedError.class, () -> {
+                assertEquals(1, resultado);
+            });
+            LOGGER.error("Exceção foi jogada, nenhum erro.");
+            LOGGER.info("Fim do Teste 2");
+        }
+    }
